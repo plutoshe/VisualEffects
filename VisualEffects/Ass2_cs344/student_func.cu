93d6b4bea6acdf8fd00be856a2c2200693744553
Pluto She <plutoshe@gmail.com>
2019-09-13 16:32:14 -0600

    c344 ass2 separateChannels

--- VisualEffects/Ass2_cs344/student_func.cu
+++ VisualEffects/Ass2_cs344/student_func.cu
@@ -152,6 +152,20 @@ void separateChannels(const uchar4* const inputImageRGBA,
   // {
   //     return;
   // }
+
+	const int2 thread_2D_pos = make_int2(blockIdx.x * blockDim.x + threadIdx.x,
+		blockIdx.y * blockDim.y + threadIdx.y);
+	const int thread_1D_pos = thread_2D_pos.y * numCols + thread_2D_pos.x;
+
+	//make sure we don't try and access memory outside the image
+	//by having any threads mapped there return early
+	if (thread_2D_pos.x >= numCols || thread_2D_pos.y >= numRows)
+    return;
+    
+  uchar4 origin = inputImageRGBA[thread_1D_pos];
+	redChannel[thread_1D_pos] = origin.x;
+	greenChannel[thread_1D_pos] = origin.y;
+	blueChannel[thread_1D_pos] = origin.z;
 }
 
 //This kernel takes in three color channels and recombines them
@@ -221,14 +235,21 @@ void your_gaussian_blur(const uchar4 * const h_inputImageRGBA, uchar4 * const d_
                         const int filterWidth)
 {
   //TODO: Set reasonable block size (i.e., number of threads per block)
-  const dim3 blockSize;
+  const dim3 blockSize((numCols - 1 / 2) + 1, (numRows - 1) / 512 + 1,1);
 
   //TODO:
   //Compute correct grid size (i.e., number of blocks per kernel launch)
   //from the image size and and block size.
-  const dim3 gridSize;
+  const dim3 gridSize(2, 512, 1);
 
   //TODO: Launch a kernel for separating the RGBA image into different color channels
+
+  separateChannels<<<gridSize, blockSize>>>(d_inputImageRGBA,
+	  numRows,
+	  numCols,
+	  d_redBlurred,
+	  d_greenBlurred,
+	  d_blueBlurred);
 
   // Call cudaDeviceSynchronize(), then call checkCudaErrors() immediately after
   // launching your kernel to make sure that you didn't make any mistakes.
