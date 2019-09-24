1b8defd7f7cead6abaf0f0bb64777f087316aaac
plutoshe <plutoshe@gmail.com>
2019-09-24 01:16:24 -0600

    update axis movement by camera axis rotation

--- VisualEffects/FogQt/CameraEngine.cpp
+++ VisualEffects/FogQt/CameraEngine.cpp
@@ -1,5 +1,8 @@
 #include "CameraEngine.h"
 
+#include <QQuaternion>
+#include <QMatrix4x4>
+
 void CameraEngine::SetCameraPosition(QVector3D i_position)
 {
 	m_position = i_position;
@@ -7,5 +10,12 @@ void CameraEngine::SetCameraPosition(QVector3D i_position)
 
 void CameraEngine::AddAxisOffset(QVector3D i_dir)
 {
-	m_position += i_dir.x() * m_right + i_dir.y() * m_up + i_dir.z() * m_direction;
+	auto rotation = QQuaternion::fromAxisAndAngle(QVector3D(0, 1, 0), CurrentYaw()) *
+		QQuaternion::fromAxisAndAngle(QVector3D(1, 0, 0), CurrentPitch());
+	QMatrix4x4 matrixRotation;
+	bool tmp;
+	matrixRotation.rotate(rotation);
+	QVector4D axis(i_dir);
+	auto a = matrixRotation * axis;
+	m_position += QVector3D(matrixRotation * axis);
 }
